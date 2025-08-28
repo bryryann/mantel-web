22e183279fba08dfb6bc0e9983952ff702ee18c6
bryryann <186111776+bryryann@users.noreply.github.com>
2025-08-28 17:52:25 -0300

    feat: create login form. - still not making api calls

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,7 +1,41 @@
+import { useForm } from 'react-hook-form';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
 import './LoginForm.css';
 
-const LoginForm = () => {
-    return <p>LoginForm</p>;
+interface FormData {
+    username: string;
+    password: string;
+}
+
+const LoginForm: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        // TODO: Submit to API.
+        console.log('submitted with data:', data);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+                label='Username'
+                {...register('username', { required: 'Username is required' })}
+                error={errors.username?.message}
+            />
+            <Input
+                label='Password'
+                type='password'
+                {...register('password', { required: 'Password is required' })}
+                error={errors.password?.message}
+            />
+            <Button type='submit'>Submit</Button>
+        </form>
+    );
 };
 
 export default LoginForm;
