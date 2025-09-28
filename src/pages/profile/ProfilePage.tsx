9e16d1a719838af1098c58a21ee87bccd804c4c1
bryryann <186111776+bryryann@users.noreply.github.com>
2025-09-28 09:23:49 -0300

    fix: handle error when fetching non-existing users

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -10,17 +10,26 @@ type ProfilePageParams = {
 
 const ProfilePage = () => {
     const [user, setUser] = useState<User | null>(null);
+    const [error, setError] = useState<string | null>(null);
     const { userID } = useParams<ProfilePageParams>();
 
     useEffect(() => {
         const fetchData = async (id: string) => {
-            const data = await fetchUser(id);
-            setUser(data.user);
-        }
+            try {
+                setError(null);
+                const data = await fetchUser(id);
+                setUser(data.user);
+            } catch (err: any) {
+                console.log(err);
+                setError(err.message || "An unknown error has occurred.");
+                setUser(null);
+            }
+        };
 
         fetchData(userID);
     }, [userID]);
 
+    if (error) return <div>Error: {error}</div>
     if (!user) return <div>Loading...</div>;
 
     return (
